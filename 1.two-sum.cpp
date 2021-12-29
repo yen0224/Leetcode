af16a1d5ce2a0264f68b109d35b70e10a158ea30
yen <yen0224@pm.me>
2021-12-29 14:51:39 +0800

    upload two solutions of 1 & 88

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -8,7 +8,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        vector<int>ans;
+        bool f=false;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if((target-nums[i])==nums[j]){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    f=true;
+                }
+            }
+            if(f==true)
+                break;
+        }
+        return ans;
     }
 };
 // @lc code=end
